69db0a79fc83b1985007ffe73102b9c239970bda
Rashmika Madaela <rashmikamadaelaofficial@gmail.com>
2026-01-11 15:26:19 +0530

    implemented text spliting and saving them to supabase vectorspace

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { createClient } from '@supabase/supabase-js';
 import fs from "fs/promises";
+process.loadEnvFile(); // Load environment variables from .env file
 
 try {
-  //const result = await fetch('profile-summary.txt')
-  //const text = await result.text()
-  const text = await fs.readFile("profile-summary.txt", "utf-8");
 
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-    separators: ['\n\n', '\n', ' ', ''], // default setting
-    chunkOverlap: 50
-  })
+    const sbApiKey = process.env.SUPABASE_API_KEY
+    const dbUrl = process.env.DATABASE_URL
+    const googleApiKey = process.env.GOOGLE_API_KEY
 
-  const output = await splitter.createDocuments([text])
-  console.log(output)
+    //const result = await fetch('profile-summary.txt')
+    //const text = await result.text()
+    const text = await fs.readFile("profile-summary.txt", "utf-8");
+
+    const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    separators: ["###", "\n\n", "\n", " "], // default setting
+    chunkOverlap: 200
+    })
+
+    const client = createClient(dbUrl, sbApiKey)
+
+    const output = await splitter.createDocuments([text])
+    //console.log(output)
+
+    await SupabaseVectorStore.fromDocuments(
+        output,
+        new GoogleGenerativeAIEmbeddings({model: "text-embedding-004"}),
+        {
+            client,
+            tableName: 'documents',
+        }
+    )
+    console.log("Data uploaded successfully!")
 } catch (err) {
-  console.log(err)
+    console.log(err)
 }
